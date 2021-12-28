a30646f3def6130602e39ad97e17e1bb3d7ac60c
TanukiMiugrar <96599613+TanukiMiugrar@users.noreply.github.com>
2021-12-28 17:11:18 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -64,16 +64,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['Try tutorial', 0.2], //From 0% to 19%
+		['baby', 0.4], //From 20% to 39%
+		['dumb', 0.5], //From 40% to 49%
+		['-1000000000 social credit', 0.6], //From 50% to 59%
+		['big stupid', 0.69], //From 60% to 68%
+		['trash', 0.7], //69%
+		['v good', 0.8], //From 70% to 79%
+		['cool', 0.9], //From 80% to 89%
+		['YEAH!', 1], //From 90% to 99%
+		['ARIANA GRANDE PEQUENA', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 	
 	public var modchartTweens:Map<String, FlxTween> = new Map<String, FlxTween>();
@@ -420,7 +420,7 @@ class PlayState extends MusicBeatState
 				}
 				
 				case 'wiik1d': //Week 1
-				var wiik1d:BGSprite = new BGSprite('wiik1d', -125, -200, 0.9, 0.9);
+				var wiik1d:BGSprite = new BGSprite('wiik1d', -600, -200, 0.9, 0.9);
 				add(wiik1d);
 				
 			case 'spooky': //Week 2
